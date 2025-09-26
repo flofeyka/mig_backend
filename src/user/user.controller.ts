5cc0fbe2b9681561008414f69aed61a2fffd38a3
Danil Bashirov <danilbashirov0@gmail.com>
2025-09-26 21:23:11 +0500

    dockerfile. update user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { UserRdo } from './rdo/user.rdo';
 import { AuthJwtGuard } from 'src/auth/auth.guard';
 import { User } from 'common/decorators/User';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @ApiOperation({ summary: 'Get user data' })
   @ApiOkResponse({ type: UserRdo })
   @UseGuards(AuthJwtGuard)
   @Get('/')
   getUserData(@User() user: UserRdo) {
     return user;
+  }
+
+  @ApiOperation({ summary: 'Update user data' })
+  @ApiOkResponse({ type: UserRdo })
+  @UseGuards(AuthJwtGuard)
+  @Put('/')
+  updateUserData(@User() user: UserRdo, @Body() dto: UpdateUserDto) {
+    return this.userService.editUserData(user.id, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user-dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRdo } from './rdo/user.rdo';
+import { User } from '@prisma/client';
+import { fillDto } from 'common/utils/fillDto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createUser(dto: CreateUserDto) {
+  async createUser(dto: CreateUserDto): Promise<User> {
     return await this.prisma.user.create({ data: dto });
   }
 
-  async findUserByEmail(email: string) {
+  async findUserByEmail(email: string): Promise<User | null> {
     return await this.prisma.user.findUnique({ where: { email } });
   }
 
-  async findUserByLogin(login: string) {
+  async findUserByLogin(login: string): Promise<User | null> {
     return await this.prisma.user.findUnique({ where: { login } });
+  }
+
+  async editUserData(id: number, dto: UpdateUserDto): Promise<UserRdo> {
+    try {
+      const user = await this.prisma.user.update({ where: { id }, data: dto });
+
+      return fillDto(UserRdo, user);
+    } catch (e) {
+      console.error(e);
+      throw new BadRequestException(
+        `User with this ${dto.email || dto.login} value already exists`,
+      );
+    }
   }
 }
