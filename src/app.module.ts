68cc871ab88aa03125e20bdac876b0e05ecdce99
Danil Bashirov <danilbashirov0@gmail.com>
2025-10-28 19:11:33 +0500

    fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,33 +8,9 @@ import { SpeechModule } from './speech/speech.module';
 import { FlowModule } from './flow/flow.module';
 import { MemberModule } from './member/member.module';
 import { PaymentModule } from './payment/payment.module';
-import * as AdminJSNestjs from '@adminjs/nestjs';
-
-const DEFAULT_ADMIN = {
-  email: 'admin@example.com',
-  password: 'password',
-};
-
-const authenticate = async (email: string, password: string) => {
-  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
-    return Promise.resolve(DEFAULT_ADMIN);
-  }
-  return null;
-};
 
 @Module({
   imports: [
-    AdminJSNestjs.AdminModule.createAdmin({
-      adminJsOptions: {
-        rootPath: '/admin',
-        resources: [],
-      },
-      auth: {
-        authenticate,
-        cookieName: 'adminjs',
-        cookiePassword: 'secret',
-      },
-    }),
     PaymentModule,
     UserModule,
     AuthModule,
