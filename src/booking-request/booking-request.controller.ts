6b87bafeac6cb2539dcae2c9c74a26b1290ce803
Danil Bashirov <danilbashirov0@gmail.com>
2025-11-09 21:54:03 +0500

    Booking request

---
+++ src/booking-request/booking-request.controller.ts
@@ -0,0 +1,108 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { SuccessRdo } from '../../common/rdo/success.rdo';
+import { BookingRequestRdo } from './rdo/request.rdo';
+import { UpdateRequestDto } from './dto/update-request.dto';
+import { BookingRequestService } from './booking-request.service';
+import { CreateRequestDto } from './dto/create-request.dto';
+import { BookingRequestsRdo } from './rdo/requests.rdo';
+import { PageDto } from '../../common/dto/page.dto';
+import { AuthJwtGuard } from '../auth/auth.guard';
+import { AdminGuard } from '../user/admin.guard';
+
+@Controller('booking-request')
+export class BookingRequestController {
+  constructor(private readonly requestService: BookingRequestService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Create a new booking request' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'New request successfully created.',
+    type: BookingRequestRdo,
+  })
+  async createRequest(
+    @Body() dto: CreateRequestDto,
+  ): Promise<BookingRequestRdo> {
+    return this.requestService.createRequest(dto);
+  }
+
+  @Get('/:id')
+  @ApiOperation({ summary: 'Retrieve a booking request by ID' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Request successfully retrieved.',
+    type: BookingRequestRdo,
+  })
+  @ApiNotFoundResponse({
+    example: new NotFoundException('Booking request not found').getResponse(),
+  })
+  @UseGuards(AuthJwtGuard, AdminGuard)
+  async fetchRequest(@Param('id') id: number): Promise<BookingRequestRdo> {
+    return this.requestService.getRequestById(+id);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Retrieve a paginated list of booking requests' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'List of requests successfully retrieved.',
+    type: BookingRequestsRdo,
+  })
+  @UseGuards(AuthJwtGuard, AdminGuard)
+  async getRequests(@Query() dto: PageDto): Promise<BookingRequestsRdo> {
+    return this.requestService.getRequests(dto.page, dto.page);
+  }
+
+  @Put('/:id')
+  @ApiOperation({ summary: 'Update an existing booking request' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Request successfully updated.',
+    type: BookingRequestRdo,
+  })
+  @ApiNotFoundResponse({
+    example: new NotFoundException('Booking request not found').getResponse(),
+  })
+  @UseGuards(AuthJwtGuard, AdminGuard)
+  async updateRequest(
+    @Param('id') id: number,
+    @Body() dto: UpdateRequestDto,
+  ): Promise<BookingRequestRdo> {
+    return this.requestService.updateRequest(+id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Delete a booking request by ID' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Request successfully deleted.',
+    type: SuccessRdo,
+  })
+  @ApiNotFoundResponse({
+    example: new NotFoundException('Booking request not found').getResponse(),
+  })
+  @UseGuards(AuthJwtGuard, AdminGuard)
+  async deleteRequest(@Param('id') id: number): Promise<SuccessRdo> {
+    return this.requestService.deleteRequest(+id);
+  }
+}
---
+++ src/booking-request/booking-request.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateRequestDto } from './dto/create-request.dto';
+import { BookingRequestRdo } from './rdo/request.rdo';
+import { PrismaService } from '../../prisma/prisma.service';
+import { fillDto } from '../../common/utils/fillDto';
+import { BookingRequestsRdo } from './rdo/requests.rdo';
+import { SuccessRdo } from '../../common/rdo/success.rdo';
+import { UpdateRequestDto } from './dto/update-request.dto';
+
+@Injectable()
+export class BookingRequestService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createRequest(dto: CreateRequestDto): Promise<BookingRequestRdo> {
+    const request = await this.prisma.bookingRequest.create({
+      data: { ...dto, date: new Date(dto.eventDate) },
+    });
+
+    return fillDto(BookingRequestRdo, request);
+  }
+
+  async getRequestById(id: number): Promise<BookingRequestRdo> {
+    const request = await this.prisma.bookingRequest.findUnique({
+      where: { id },
+    });
+
+    if (!request) throw new NotFoundException('Booking request not found');
+
+    return fillDto(BookingRequestRdo, request);
+  }
+
+  async getRequests(
+    page: number = 1,
+    limit: number = 15,
+  ): Promise<BookingRequestsRdo> {
+    const where = {};
+
+    const [requests, total] = await Promise.all([
+      this.prisma.bookingRequest.findMany({
+        where,
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      this.prisma.bookingRequest.count({ where }),
+    ]);
+
+    return fillDto(BookingRequestsRdo, { requests, total });
+  }
+
+  async deleteRequest(id: number): Promise<SuccessRdo> {
+    try {
+      await this.prisma.bookingRequest.delete({ where: { id } });
+
+      return fillDto(SuccessRdo, { success: true });
+    } catch (e) {
+      console.error(`[${id}]: Cannot delete the booking request`, e);
+      throw new NotFoundException('Booking request not found');
+    }
+  }
+
+  async updateRequest(id: number, dto: UpdateRequestDto) {
+    try {
+      const request = await this.prisma.bookingRequest.update({
+        where: { id },
+        data: {
+          ...dto,
+          ...(dto.eventDate && { date: new Date(dto.eventDate) }),
+        },
+      });
+
+      return fillDto(BookingRequestRdo, request);
+    } catch (e) {
+      console.error(`[${id}]: Cannot update the booking request`, e);
+      throw new NotFoundException('Booking request not found');
+    }
+  }
+}
